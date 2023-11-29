85e99e069190a5f705f618b00ea74239dd2d0011
BRp99 <124196319+BRp99@users.noreply.github.com>
2023-11-29 19:04:20 +0000

    remove code from HamburgerMenu

--- portfolio/src/components/Layout/Header/HamburgerMenu/HamburgerMenu.tsx
+++ portfolio/src/components/Layout/Header/HamburgerMenu/HamburgerMenu.tsx
@@ -1,42 +1,12 @@
-import { useState, useEffect } from "react"
+import React, { useState } from "react"
 import styles from "./HamburgerMenu.module.css"
 
 export default function HamburgerMenu() {
-  useEffect(() => {
-    const menuBtn = document.querySelector("." + styles.hamburger)
-    const mobileMenu = document.querySelector("." + styles.mobile_nav)
+  const [isMenuActive, setMenuActive] = useState(false)
 
-    const handleClick = () => {
-      if (menuBtn && mobileMenu) {
-        menuBtn.classList.toggle("is-active")
-        mobileMenu.classList.toggle("is-active")
-      }
-    }
+  const toggleMenu = () => {
+    setMenuActive((prevMenuActive) => !prevMenuActive)
+  }
 
-    if (menuBtn) {
-      menuBtn.addEventListener("click", handleClick)
-    }
-
-    return () => {
-      // Remover o event listener ao desmontar o componente
-      if (menuBtn) {
-        menuBtn.removeEventListener("click", handleClick)
-      }
-    }
-  }, [])
-
-  return (
-    <div>
-      <button className={styles.hamburger}>
-        <div className={styles.bar}></div>
-      </button>
-
-      <nav className={styles.mobile_nav}>
-        <a href="#">Home</a>
-        <a href="#">Services</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
-      </nav>
-    </div>
-  )
+  return <div></div>
 }
